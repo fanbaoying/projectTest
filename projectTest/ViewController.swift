e506ba0ae886e6bde8ce227d167cbdaefbf50181
ZhanFei <1479162730@qq.com>
2017-04-10 20:38:44 +0800

    Initial Commit

---
+++ projectTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  projectTest
+//
+//  Created by 范保莹 on 2017/4/10.
+//  Copyright © 2017年 FBY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
